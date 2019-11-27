621457b728bf8ecc4d125ecfc82be58a6ef90399
benjaminli7 <47728809+benjaminli7@users.noreply.github.com>
2019-11-27 17:40:15 +0100

    first commit

---
+++ GestionnaireBDD/GstBdd.cs
@@ -0,0 +1,108 @@
+﻿using ClassesMetier;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GestionnaireBDD
+{
+    public class GstBdd
+    {
+        MySqlConnection cnx;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public GstBdd()
+        {
+            string driver = "server=localhost;user id=root;password=;database=reservationsalle";
+            cnx = new MySqlConnection(driver);
+            cnx.Open();
+        }
+
+        public List<Manifestation> GetAllManifestations()
+        {
+            List<Manifestation> lstManif = new List<Manifestation>();
+            cmd = new MySqlCommand("select * from manifestation inner join salle on numSalle = idSalle", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Salle s = new Salle()
+                {
+                    IdSalle = Convert.ToInt32(dr[4]),
+                    NomSalle = dr[6].ToString(),
+                    NbPlaces = Convert.ToInt32(dr[7])
+                };
+
+                Manifestation m = new Manifestation()
+                {
+                    IdManif = Convert.ToInt32(dr[0]),
+                    NomManif = dr[1].ToString(),
+                    DateDebutManif = dr[2].ToString(),
+                    DateFinManif = dr[3].ToString(),
+                    LaSalle = s
+                };
+                lstManif.Add(m);
+            }
+            dr.Close();
+            return lstManif;
+        }
+
+        public List<Place> GetAllPlacesByIdManifestation(int idManif,int idSalle)
+        {
+            List<Place> lstPlace = new List<Place>();
+            cmd = new MySqlCommand("select numPlace, numTarif, libre, prix from manifestation inner join occuper on idManif = numManif inner join place on numPlace = place.idPlace inner join tarif on place.numTarif = tarif.idTarif where idManif = " + idManif + " and place.numSalle = " + idSalle + ";", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                bool a;
+                char etat;
+                if(Convert.ToInt32(dr[2]) == 1)
+                {
+                    a = true;
+                    etat = 'o';
+                }
+                else
+                {
+                    a = false;
+                    etat = 'l';
+                }
+
+                Place p = new Place()
+                {
+                    IdPlace = Convert.ToInt32(dr[0]),
+                    CodeTarif = Convert.ToChar(dr[1]),
+                    Occupee = a,
+                    Prix = Convert.ToDouble(dr[3]),
+                    Etat = etat
+                };
+                lstPlace.Add(p);
+            }
+            dr.Close();
+            return lstPlace;
+        }
+
+        public List<Tarif> GetAllTarifs()
+        {
+            List<Tarif> lstTarif = new List<Tarif>();
+            cmd = new MySqlCommand("select * from tarif", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Tarif t = new Tarif()
+                {
+                    IdTarif = Convert.ToChar(dr[0]),
+                    NomTarif = dr[1].ToString(),
+                    Prix = Convert.ToDouble(dr[2])
+                };
+                lstTarif.Add(t);
+            }
+            dr.Close();
+            return lstTarif;
+        }
+
+        public void ReserverPlace(int idPlace, int idSalle,int idManif)
+        {
+            cmd = new MySqlCommand("update occuper SET libre = 1 where numManif = " + idManif + " and numPlace = " + idPlace + " and numSalle = " + idSalle + ";", cnx);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ UWPGestionSalles/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GestionnaireBDD;
+using ClassesMetier;
+using Windows.UI.Popups;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPGestionSalles
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        GstBdd bdd;
+        List<Place> lesPlaces;
+        double montant;
+        private async void btnReserver_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstManifs.SelectedItem != null){
+                if(lesPlaces.Count > 0)
+                {
+                    foreach (Place p in lesPlaces)
+                    {
+                        bdd.ReserverPlace(p.IdPlace, (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle, (lstManifs.SelectedItem as Manifestation).IdManif);
+                    }
+                    var dialog3 = new MessageDialog("Vos places ont bien été réservées");
+                    await dialog3.ShowAsync();
+                    gvPlaces.ItemsSource = null;
+                    gvPlaces.ItemsSource = bdd.GetAllPlacesByIdManifestation((lstManifs.SelectedItem as Manifestation).IdManif, (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle);
+                }
+                else
+                {
+                    var dialog1 = new MessageDialog("Sélectionnez une place");
+                    await dialog1.ShowAsync();
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("Sélectionnez une manifestation");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            bdd = new GstBdd();
+            lstManifs.ItemsSource = bdd.GetAllManifestations();
+            lstTarifs.ItemsSource = bdd.GetAllTarifs();
+        }
+
+        private void lstManifs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            montant = 0;
+            txtTotal.Text = montant.ToString();
+            lesPlaces = new List<Place>();
+            gvPlaces.ItemsSource = null;
+            gvPlaces.ItemsSource = bdd.GetAllPlacesByIdManifestation((lstManifs.SelectedItem as Manifestation).IdManif, (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle);
+            txtNumSalle.Text = (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle.ToString();
+            txtNbPlaces.Text = (lstManifs.SelectedItem as Manifestation).LaSalle.NbPlaces.ToString();
+            txtNomSalle.Text = (lstManifs.SelectedItem as Manifestation).LaSalle.NomSalle;
+        }
+
+        private async void gvPlaces_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(gvPlaces.SelectedItem != null)
+            {
+                if ((gvPlaces.SelectedItem as Place).Occupee == true)
+                {
+                    var dialog4 = new MessageDialog("La place est déjà occupée!");
+                    await dialog4.ShowAsync();
+                }
+                else
+                {
+                    if((gvPlaces.SelectedItem as Place).Etat == 'l')
+                    {
+                        (gvPlaces.SelectedItem as Place).Etat = 'r';
+                        lesPlaces.Add(gvPlaces.SelectedItem as Place);
+                        montant += Convert.ToDouble((gvPlaces.SelectedItem as Place).Prix);
+                        txtTotal.Text = montant.ToString();
+                    }
+                    else
+                    {
+                        (gvPlaces.SelectedItem as Place).Etat = 'l';
+                        lesPlaces.Remove(gvPlaces.SelectedItem as Place);
+                        montant -= Convert.ToDouble((gvPlaces.SelectedItem as Place).Prix);
+                        txtTotal.Text = montant.ToString();
+                    }
+                }
+            }
+
+
+        }
+    }
+}
